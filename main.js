904256b65cc51ce4830c766c2f20243446296aaf
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-02 01:22:30 -0400

    how long is my string

--- main.js
+++ main.js
@@ -18,7 +18,16 @@ function isItShort(str) {
 
 }
 
-function howLongIsMyString() {
+function howLongIsMyString(str) {
+  if (str.length > 20) {
+    return "That's a long string";
+  }
+  else if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!";
+  }
+  else if (str.length < 10) {
+    return "That's a small string!"; 
+  }
 
 }
 
